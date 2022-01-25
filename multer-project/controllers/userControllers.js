d7fa951b45bc941cf7677a6ae44ab31340ce0c3c
bohyeon <980926a@gmail.com>
2022-01-25 12:58:59 +0900

    multer 공부

--- multer-project/controllers/userControllers.js
+++ multer-project/controllers/userControllers.js
@@ -15,6 +15,8 @@ module.exports = {
     uploadImages : async (req,res)=>{
         const image = req.files;
         const path = image.map(img => img.path);
+        console.log(req.files)
+        // console.log(req.body.receipe_list)
         if(image === undefined){
             return res.status(400).send(defaultRes.successFalse(statusCode.NO_CONTENT),resMessage.INPUT_SUCCESS);
         }
